4e8b5af68b35b2de69a76ac2a90bd98b6701f9a0
Hitesh <hiteshspatil99@gmail.com>
2021-11-17 00:22:01 +0530

    [Hitesh] add : Creating a Contacts in Address Book

---
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice Number to Execute the Address Program Press- 1-Contacts, 2-Add Contact, 3-Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Contact Information in Format of FirstName, LastName, Addr, City, State, Zip, Ph.No, Email.");
+                        StartContact contact = new StartContact();
+                        contact.FirstName = Console.ReadLine();
+                        contact.LastName = Console.ReadLine();
+                        contact.Address = Console.ReadLine();
+                        contact.City = Console.ReadLine();
+                        contact.State = Console.ReadLine();
+                        contact.Zip = Console.ReadLine();
+                        contact.PhoneNumber = Console.ReadLine();
+                        contact.Email = Console.ReadLine();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ AddressBookSystem/AddressBookSystem/StartContact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class StartContact // This Model Class is Only Responisible for Changing Purpose
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
